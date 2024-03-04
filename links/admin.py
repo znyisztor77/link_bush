bb6d899b1018ed340f02fd56f45f1cbfd857ba04
Nyisztor Zoltan <znyisztor77@gmail.com>
2024-03-04 17:06:10 +0100

    03.04.  links app and models

---
+++ links/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import MyLink
+# Register your models here.
+
+admin.site.register(MyLink)
---
+++ links/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class MyLink(models.Model):
+    title = models.CharField(max_length = 255)
+    url = models.URLField()
+    visible = models.BooleanField(default = True)
+    user = models.ForeignKey(User , on_delete = models.CASCADE)
+    created = models.DateTimeField( auto_now_add = True)
+
+    def __str__(self):
+        return self.title
